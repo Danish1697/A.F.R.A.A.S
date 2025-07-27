1859ac9d98a9cefcd4da1476e54bebe216fa6dad
Danish1697 <148640787+Danish1697@users.noreply.github.com>
2025-07-27 13:56:29 +0530

    alpha.py

--- AFRAAS/alpha.py
+++ AFRAAS/alpha.py
@@ -1,8 +1,1 @@
-import os
-import face_recognition
-import cv2
-import numpy as np
-import datetime
 
-a= os.path.dirname(os.path.abspath(__file__))  # Current script directory
-path = os.path.join(a, 'ImagesAttendance')  # Full path to ImagesAttendancea
